acedc5dd0de7fa819148dc33974af36fd49ddc14
niina112 <91963048+niina112@users.noreply.github.com>
2024-11-21 14:20:49 +0400

    updated

--- script.js
+++ script.js
@@ -6,25 +6,25 @@ console.log(text1)
 
 for (let i = 0; i < text1.length; i++) {
     if (text1[i].length >= 5) {
-      text1[i] = "LongWord"; 
+        text1[i] = "LongWord";
     } else {
-      text1[i] = "ShortWord"; 
+        text1[i] = "ShortWord";
     }
-  }
-  
-  let modString = text1.join(' ');
-  console.log("String:", modString);
+}
+
+let modString = text1.join(' ');
+console.log("String:", modString);
 
 
-  let words = text.split(' ');
+let words = text.split(' ');
 let count = 0;
 let index = 0;
 
 while (index < words.length) {
-  if (words[index].includes('a')) {
-    count++;
-  }
-  index++;
+    if (words[index].includes('a')) {
+        count++;
+    }
+    index++;
 }
 console.log(`Number of "a" in this text : ${count}`);
 let words1 = modString.split(' ');
@@ -37,6 +37,6 @@ let modIndex = 0;
 
 console.log("Words from text1:");
 do {
-  console.log(text1[modIndex]);
-  modIndex++;
+    console.log(text1[modIndex]);
+    modIndex++;
 } while (modIndex < text1.length);
